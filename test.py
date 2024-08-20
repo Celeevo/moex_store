a64215c2a6dc9098ee9fd28a4184ac229009ba75
AlekseyKorn <korn2052@gmail.com>
2024-08-20 06:28:58 +0300

    20-08-24

---
+++ test.py
@@ -0,0 +1,26 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import backtrader as bt
+from datetime import datetime
+from moex_store import MoexStore
+
+
+def runstrat():
+    cerebro = bt.Cerebro(stdstats=False)
+    cerebro.addstrategy(bt.Strategy)
+
+    store = MoexStore(write_to_file=True)
+    fd = "13-12-2023"
+    td = '15-03-2024'
+    tf='15m'
+    today = datetime.today()
+    for tiker in ('SiM4', 'RiH4'):
+        data = store.get_data(sec_id=tiker, fromdate=fd, todate=td, tf=tf)
+        cerebro.adddata(data)
+
+    cerebro.run()
+    cerebro.plot(style='bar')
+
+
+if __name__ == '__main__':
+    runstrat()
